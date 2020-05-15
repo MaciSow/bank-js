10d8e09676e284e39c654a9ff6144d2f2e2c3b8b
Maciej Sowiński <>
2020-05-15 13:02:24 +0200

    sorting

--- js/History.js
+++ js/History.js
@@ -13,35 +13,73 @@ export class History {
 
     show() {
         slideReset(this.historyContainer);
-        this.fillTable();
+        this.fillTable(this.makeTransactionList());
+        this.listenSortClick();
     }
 
-    fillTable() {
-        this.tableBody.innerHTML = this.generateTableBody();
-        console.log('generateTableBody: ', this.generateTableBody());
 
+    fillTable(transactionList) {
+        this.tableBody.innerHTML = this.generateTableBody(transactionList);
     }
 
-    generateTableBody() {
-        let transactionItems = '';
+    makeTransactionList() {
+        let transactionItems = [];
         this.accounts.forEach(account => {
             account.transactions.forEach(transaction => {
-                const data = {
+                transactionItems.push({
                     accountNumber: account.accountNumber,
                     amount: transaction.amount,
-                    date: shortFormatDate(transaction.date)
-                };
-                transactionItems += this.generateRow(data);
+                    date: transaction.date
+                });
             })
         })
         return transactionItems;
+    }
+
+    generateTableBody(transactionItems) {
+        let htmlBody = '';
+        transactionItems.forEach(item => htmlBody += this.generateRow(item))
+        return htmlBody;
     }
 
     generateRow(data) {
         return `<tr>
         <td>${data.accountNumber}</td>
         <td>${data.amount}</td>
-        <td>${data.date}</td>
+        <td>${shortFormatDate(data.date)}</td>
         </tr>`
+    }
+
+    listenSortClick() {
+        const buttons = historyContainer.querySelectorAll('.js-sort-transaction');
+        buttons.forEach(item => item.addEventListener('click', evt => this.toggleSortDirection(evt)))
+    }
+
+    toggleSortDirection(evt) {
+        const btn = evt.target;
+        const sortColumn = btn.dataset.columnSort;
+        const sortDirection = btn.dataset.sortDirection || null;
+        const sortingData = this.makeTransactionList();
+
+        this.resetSortDirections();
+
+        switch (sortDirection) {
+            case 'down':
+                 break;
+            case 'up':
+                sortingData.sort((a, b) => b[sortColumn] - a[sortColumn]);
+                btn.dataset.sortDirection = 'down';
+                break;
+            default:
+                sortingData.sort((a, b) => a[sortColumn] - b[sortColumn]);
+                btn.dataset.sortDirection = 'up';
+        }
+       
+        this.fillTable(sortingData);
+    }
+
+    resetSortDirections() {     
+        const allColumns = historyContainer.querySelectorAll('.js-sort-transaction');
+        allColumns.forEach(item => item.dataset.sortDirection = '')
     }
 }
